b887e810615a3382e5a321dc1bd80fa910bb7411
Victor <victorgrinan@gmail.com>
2024-02-09 12:26:48 +0200

    make fields nullable

--- portfolio_project/chat_server/models.py
+++ portfolio_project/chat_server/models.py
@@ -14,8 +14,8 @@ class Server(models.Model):
     #on_delete=models.CASCADE read documentation to find out what todo on delete:
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="server_category")
-    description = models.CharField(max_length = 255, null=True)
-    member = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    description = models.CharField(max_length = 255, blank=True, null=True)
+    member = models.ManyToManyField(settings.AUTH_USER_MODEL, null=True)
 
     def __str__(self):
         return self.name
