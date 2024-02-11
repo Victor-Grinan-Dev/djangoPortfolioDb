b3eefba1f0ecafc4eaf22d367437be5ea1075b8c
Victor <victorgrinan@gmail.com>
2024-02-11 13:59:13 +0200

    filter by user = true

--- portfolio_project/chat_server/views.py
+++ portfolio_project/chat_server/views.py
@@ -11,9 +11,19 @@ class ServerListViewSet(viewsets.ViewSet):
 
     def list(self, request):
         category = request.query_params.get("category")
-        
+        qty = request.query_params.get("qty")
+        by_user = request.query_params.get("by_user") == 'true' 
+        print(by_user)
+
         if category:
-            self.queryset = self.queryset.filter(category__name =category)
+            self.queryset = self.queryset.filter(category__name=category)
+
+        if qty:
+            self.queryset = self.queryset[:int(qty)]
+
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)
 
         serializer = ServerSerializer(self.queryset, many=True)
 
