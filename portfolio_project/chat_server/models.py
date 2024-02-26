d749b1e15c7f45ea8c05e824b76f1309988e452c
Victor <victorgrinan@gmail.com>
2024-02-26 10:43:06 +0200

    delete image along with the del of category

--- portfolio_project/chat_server/models.py
+++ portfolio_project/chat_server/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
 
 def category_icon_upload(instance, filename):
     return f"category/{instance.id}/category_icon/{filename}"
@@ -11,6 +13,22 @@ class Category(models.Model):
         upload_to=category_icon_upload, 
         null=True, 
         blank=True)
+    
+    def save(self, *args, **kwargs):
+        """keep only one icon per category"""
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="chat_server.Category")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
