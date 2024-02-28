38854a89bc84f5a5f1444e573e94651cb8ea6754
Victor <victorgrinan@gmail.com>
2024-02-28 11:34:23 +0200

    MUI installed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import Home from './pages/Home'
+import { Route, createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,} from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <Home></Home>
-      <h1 onClick={()=>setCount(count+1)}>Hello {count}</h1>
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<Home/>} />
+    </Route>
   )
-}
+);
+
+const App: React.FC = () => {
+    return <RouterProvider router={router} />;
+  }
+
 
 export default App
